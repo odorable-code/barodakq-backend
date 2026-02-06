60153f06f54a7368453584389346ba8f3ed35902
odorable-code <voidkknd17@gmail.com>
2026-02-06 16:45:05 +0900

    first comment

---
+++ src/main/java/kr/yeyakhaezo/barodakq/BarodakqApplication.java
@@ -0,0 +1,13 @@
+package kr.yeyakhaezo.barodakq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarodakqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarodakqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/yeyakhaezo/barodakq/BarodakqApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yeyakhaezo.barodakq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarodakqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
